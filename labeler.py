f5d6cb5f6b2728c2abfd1656b365dbfc44470599
Ethan Saxenian <ethansaxenian@gmail.com>
2021-03-12 17:33:23 -0500

    add labels!

---
+++ labeler.py
@@ -0,0 +1,41 @@
+import os
+
+from xlwt import Workbook
+
+EXTENSION_TO_LANGUAGE = {
+    ".c": "C",
+    ".cpp": "C++",
+    ".go": "Go",
+    ".hs": "Haskell",
+    ".java": "Java",
+    ".js": "JavaScript",
+    ".julia": "Julia",
+    ".pl": "Perl",
+    ".py": "Python",
+    ".rb": "Ruby",
+    ".rust": "Rust",
+    ".scala": "Scala"
+}
+
+if __name__ == '__main__':
+    wb = Workbook()
+    labels = wb.add_sheet('labels', cell_overwrite_ok=True)
+    labels.write(0, 0, "Filename")
+    labels.write(0, 1, "Language")
+    labels.write(0, 3, "Supported Languages")
+
+    # list supported languages
+    for row, (ext, lang) in enumerate(EXTENSION_TO_LANGUAGE.items()):
+        labels.write(row + 1, 3, ext)
+        labels.write(row + 1, 4, lang)
+
+    # label all supported files
+    row = 1
+    for lang in EXTENSION_TO_LANGUAGE.values():
+        for filename in os.listdir(f'lang/{lang}'):
+            extension = filename[filename.index("."):]
+            labels.write(row, 0, filename)
+            labels.write(row, 1, EXTENSION_TO_LANGUAGE[extension])
+            row += 1
+
+    wb.save('labels.xls')
