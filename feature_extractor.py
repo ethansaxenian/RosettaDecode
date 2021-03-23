5e2ae67a141278156274be9de3360c8b16371a31
Ethan Saxenian <ethansaxenian@gmail.com>
2021-03-22 22:18:30 -0400

    add binary counts option to feature set

--- feature_extractor.py
+++ feature_extractor.py
@@ -5,11 +5,11 @@ Preliminary Features List:
     - percent of all chars that are special
     - most common line endings (use all characters mapping or unicode with ord())
     - most common words (use vocabulary.jsonl)
-    - n-grams
+    - n-grams?
 
-Bag-of-words model?
-    - use reserved words in the languages?
-    - use special characters?
+Bag-of-words model
+    - use reserved words in the languages
+    - use special characters
 """
 import json
 import os
@@ -30,13 +30,13 @@ SPECIAL_CHAR_NAMES = {"'": "squote", "~": "tilde", "`": "backtick", "!": "exclai
 
 SPECIALS = list(SPECIAL_CHAR_NAMES.keys())
 
-ALL_CHAR_MAPPING = {"'": 0, '~': 1, '`': 2, '!': 3, '@': 4, '#': 5, '$': 6, '%': 7, '^': 8, '&': 9, '*': 10, '(': 11,
-                    ')': 12, '-': 13, '+': 14, '=': 15, '[': 16, ']': 17, '{': 18, '}': 19, '|': 20, ';': 21, ':': 22,
-                    '"': 23, ',': 24, '.': 25, '<': 26, '>': 27, '/': 28, '?': 29, '\\': 30, '...': 31, 'a': 32,
-                    'b': 33, 'c': 34, 'd': 35, 'e': 36, 'f': 37, 'g': 38, 'h': 39, 'i': 40, 'j': 41, 'k': 42, 'l': 43,
-                    'm': 44, 'n': 45, 'o': 46, 'p': 47, 'q': 48, 'r': 49, 's': 50, 't': 51, 'u': 52, 'v': 53, 'w': 54,
-                    'x': 55, 'y': 56, 'z': 57, '_': 58, '1': 59, '2': 60, '3': 61, '4': 62, '5': 63, '6': 64, '7': 65,
-                    '8': 66, '9': 67, '0': 68}
+CHAR_MAPPING = {"'": 0, '~': 1, '`': 2, '!': 3, '@': 4, '#': 5, '$': 6, '%': 7, '^': 8, '&': 9, '*': 10, '(': 11,
+                ')': 12, '-': 13, '+': 14, '=': 15, '[': 16, ']': 17, '{': 18, '}': 19, '|': 20, ';': 21, ':': 22,
+                '"': 23, ',': 24, '.': 25, '<': 26, '>': 27, '/': 28, '?': 29, '\\': 30, '...': 31, 'a': 32, 'b': 33,
+                'c': 34, 'd': 35, 'e': 36, 'f': 37, 'g': 38, 'h': 39, 'i': 40, 'j': 41, 'k': 42, 'l': 43, 'm': 44,
+                'n': 45, 'o': 46, 'p': 47, 'q': 48, 'r': 49, 's': 50, 't': 51, 'u': 52, 'v': 53, 'w': 54, 'x': 55,
+                'y': 56, 'z': 57, '_': 58, '1': 59, '2': 60, '3': 61, '4': 62, '5': 63, '6': 64, '7': 65, '8': 66,
+                '9': 67, '0': 68}
 
 RESERVED_KEYWORDS = ['abstract', 'and', 'as', 'assert', 'begin', 'bool', 'boolean', 'break', 'byte', 'case', 'catch',
                      'chan', 'char', 'class', 'const', 'continue', 'def', 'default', 'defer', 'del', 'delete',
@@ -44,12 +44,12 @@ RESERVED_KEYWORDS = ['abstract', 'and', 'as', 'assert', 'begin', 'bool', 'boolea
                      'except', 'exit', 'extends', 'false', 'final', 'finally', 'float', 'for', 'foreach', 'friend',
                      'from', 'func', 'function', 'global', 'go', 'goto', 'if', 'implements', 'import', 'in', 'inline',
                      'instanceof', 'int', 'interface', 'is', 'lambda', 'let', 'local', 'long', 'map', 'module', 'my',
-                     'namespace', 'new', 'nil', 'none', 'not', 'null', 'of', 'operator', 'or', 'package',
-                     'pass', 'print', 'private', 'proc', 'protected', 'public', 'qualified', 'raise', 'range', 'ref',
-                     'require', 'rescue', 'return', 'self', 'short', 'signed', 'sizeof', 'static', 'struct',
-                     'super', 'switch', 'template', 'then', 'this', 'throw', 'throws', 'true', 'try', 'type', 'typedef',
-                     'typename', 'typeof', 'undef', 'undefined', 'unless', 'unsigned', 'until', 'use', 'using', 'var',
-                     'virtual', 'void', 'when', 'where', 'while', 'with', 'yield']
+                     'namespace', 'new', 'nil', 'none', 'not', 'null', 'of', 'operator', 'or', 'package', 'pass',
+                     'print', 'private', 'proc', 'protected', 'public', 'qualified', 'raise', 'range', 'ref', 'require',
+                     'rescue', 'return', 'self', 'short', 'signed', 'sizeof', 'static', 'struct', 'super', 'switch',
+                     'template', 'then', 'this', 'throw', 'throws', 'true', 'try', 'type', 'typedef', 'typename',
+                     'typeof', 'undef', 'undefined', 'unless', 'unsigned', 'until', 'use', 'using', 'var', 'virtual',
+                     'void', 'when', 'where', 'while', 'with', 'yield']
 
 
 def remove_spaces(code: str) -> str:
@@ -80,54 +80,61 @@ def pct_specials(code: str) -> float:
     return len(find_special_characters(code)) / len(remove_spaces(code))
 
 
-def generate_feature_set(code: str) -> dict[str: int]:
-    feature_set = {}
-
-    specials_count = Counter(find_special_characters(code))
+def extract_features(code: str, binary_counts: bool = False) -> dict[str: int]:
+    features_dict = {}
 
     bag_of_words = Counter(find_words(code))
     for keyword in RESERVED_KEYWORDS:
-        feature_set[keyword] = bag_of_words[keyword]
+        num = bag_of_words[keyword]
+        features_dict[keyword] = int(bool(num)) if binary_counts else num
 
+    specials_count = Counter(find_special_characters(code))
     num_specials = len(list(specials_count.elements()))
     for char in SPECIALS:
-        feature_set[f'num_{SPECIAL_CHAR_NAMES[char]}'] = specials_count[char]
+        num = specials_count[char]
+        features_dict[f'num_{SPECIAL_CHAR_NAMES[char]}'] = int(bool(num)) if binary_counts else num
         percent_specials = (specials_count[char] / num_specials) if num_specials > 0 else 0
-        feature_set[f'percent_{SPECIAL_CHAR_NAMES[char]}'] = percent_specials
+        features_dict[f'percent_{SPECIAL_CHAR_NAMES[char]}'] = percent_specials
 
-    feature_set['percent_specials'] = pct_specials(code)
+    features_dict['percent_specials'] = pct_specials(code)
 
-    for rank, (char, _) in enumerate(count_line_endings(code).most_common(5)):
-        feature_set[f'line_ending_{rank+1}'] = ALL_CHAR_MAPPING[char]
+    most_common_ending = count_line_endings(code).most_common(1)[0][0]
+    features_dict['most_frequent_line_ending'] = CHAR_MAPPING[most_common_ending]
 
-    return feature_set
+    return features_dict
 
 
-def compile_dataset():
+def compile_dataset(lowercase: bool = True, binary_counts: bool = False):
     with open("data/features_data.jsonl", "w") as outfile, open("data/file_paths.jsonl", "r") as infile:
         for line in infile:
             info = json.loads(line)
-            code = read_file(info["path"])
-            data = {
-                "name": os.path.basename(info["path"]),
-                "features": generate_feature_set(code),
-                "lang": get_language_from_filename(info["path"]),
-                # "code": code,
-            }
+            data = parse_file(info["path"], lowercase, binary_counts)
             json.dump(data, outfile)
             outfile.write("\n")
 
 
-def read_file(path: str) -> str:
+def read_file(path: str, lowercase: bool = True) -> str:
     with open(path, "r") as file:
-        return unidecode.unidecode(file.read().lower())
+        code = file.read()
+        if lowercase:
+            code = code.lower()
+        return unidecode.unidecode(code)
+
+
+def parse_file(path: str, binary_counts: bool = False, lowercase: bool = True) -> dict[str: int]:
+    code = read_file(path, lowercase)
+    data = {
+        "name": os.path.basename(path),
+        "features": extract_features(code, binary_counts),
+        "lang": get_language_from_filename(path),
+        # "code": code,
+    }
+    return data
 
 
 if __name__ == '__main__':
     # generate_file_paths()
-    compile_dataset()
-    # with open("lang/Python/conditional-structures-1.py", "r") as file:
-    #     code = file.read().lower()
-    #     print(generate_feature_set(code))
-    #     print(count_line_endings(code))
-    # print(len(RESERVED_KEYWORDS))
+    # compile_dataset()
+    #
+    print(parse_file("lang/Python/conditional-structures-1.py", binary_counts=True))
+
